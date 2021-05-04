6c0d25a66c0bcf734ff9808b5f6ab18746e07804
Tomasz Zboralski <tomasz.zboralski@gmail.com>
2021-05-04 11:57:37 +0200

    first commit

---
+++ src/main/java/com/crud/tacho/TachoApplication.java
@@ -0,0 +1,13 @@
+package com.crud.tacho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TachoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TachoApplication.class, args);
+    }
+
+}
